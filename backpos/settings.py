db52a586f2438f90d2418ea9c330d0188d2827db
Cesar Mayta <cesarmayta@gmail.com>
2021-09-08 21:40:25 -0500

    d03

--- backpos/settings.py
+++ backpos/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = ''
-SECRET_KEY = config('SECRET_KEY')  
+SECRET_KEY = 'django-insecure-9!av8ocxogu)djo3nh8-6xt60(_q810i%%!)j)f6eco4)jss9k'
+#SECRET_KEY = config('SECRET_KEY')  
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
